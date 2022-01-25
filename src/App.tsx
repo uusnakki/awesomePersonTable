b7ef908a786b7d017a2f93529fa54752218ec592
Joonas Uusnäkki <joonas.uusnakki@gmail.com>
2022-01-25 17:56:10 +0200

    css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import './App.css';
 //import PersonTable from './components/PersonTable'
 import PersonTable from './components/PersonTable'
+import HeaderComponent from './components/HeaderComponent'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          Person table application
-        </h1>
-        <p>by Joonas Uusnäkki</p>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/joonas-uusn%C3%A4kki-82300a198/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Get to know my professional me!
-        </a>
-        <br />
+        <HeaderComponent />
         <PersonTable />
-      </header>
     </div>
   );
 }
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,25 @@
+import '../App.css';
+
+
+
+const Header = () => {
+
+    return (
+        <div className="headerComponent">
+            <h1>
+                Person table application
+            </h1>
+            <p>by Joonas Uusnäkki</p>
+            <a
+                className="App-link"
+                href="https://www.linkedin.com/in/joonas-uusn%C3%A4kki-82300a198/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Get to know my professional me!
+            </a>
+        </div>
+    );
+};
+
+export default Header;
